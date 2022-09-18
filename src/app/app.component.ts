c7894c3c8af14d3901eac707261d2cb59ef9f447
Lotar Medina <lotar.94@gmail.com>
2022-09-18 00:56:34 -0300

    update principal view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  diarySpend:number = 6.599;
+
   title = 'gastos-hormiga-pwa';
+
+  public showModal():void {
+    console.log("llega");
+    // Get the modal
+    let modal = document.getElementById("myModal");
+
+    if (modal != null) {
+      modal.style.display = "block";  
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        if (modal != null) {
+          modal.style.display = "none"
+        }
+      }
+    }
+    
+  }
 }
