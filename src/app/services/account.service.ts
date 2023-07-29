00c96381ad26ced7fbf1f216b96562f39bd93b51
Lotar <lotar.94@gmail.com>
2023-07-29 16:10:54 -0400

    build and deploy app configs

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -27,13 +27,13 @@ export class AccountService {
     }
 
     login(username: string, password: string) {
-        return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, { username, password })
-            .pipe(map(user => {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(user));
-                this.userSubject.next(user);
-                return user;
-            }));
+        // return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, { username, password })
+        //     .pipe(map(user => {
+        //         // store user details and jwt token in local storage to keep user logged in between page refreshes
+        //         localStorage.setItem('user', JSON.stringify(user));
+        //         this.userSubject.next(user);
+        //         return user;
+        //     }));
     }
 
     logout() {
@@ -44,41 +44,41 @@ export class AccountService {
     }
 
     register(user: User) {
-        return this.http.post(`${environment.apiUrl}/users/register`, user);
+        // return this.http.post(`${environment.apiUrl}/users/register`, user);
     }
 
     getAll() {
-        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+        // return this.http.get<User[]>(`${environment.apiUrl}/users`);
     }
 
     getById(id: string) {
-        return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+        // return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
     }
 
     update(id: string, params: any) {
-        return this.http.put(`${environment.apiUrl}/users/${id}`, params)
-            .pipe(map(x => {
-                // update stored user if the logged in user updated their own record
-                if (id == this.userValue?.id) {
-                    // update local storage
-                    const user = { ...this.userValue, ...params };
-                    localStorage.setItem('user', JSON.stringify(user));
+        // return this.http.put(`${environment.apiUrl}/users/${id}`, params)
+        //     .pipe(map(x => {
+        //         // update stored user if the logged in user updated their own record
+        //         if (id == this.userValue?.id) {
+        //             // update local storage
+        //             const user = { ...this.userValue, ...params };
+        //             localStorage.setItem('user', JSON.stringify(user));
 
-                    // publish updated user to subscribers
-                    this.userSubject.next(user);
-                }
-                return x;
-            }));
+        //             // publish updated user to subscribers
+        //             this.userSubject.next(user);
+        //         }
+        //         return x;
+        //     }));
     }
 
     delete(id: string) {
-        return this.http.delete(`${environment.apiUrl}/users/${id}`)
-            .pipe(map(x => {
-                // auto logout if the logged in user deleted their own record
-                if (id == this.userValue?.id) {
-                    this.logout();
-                }
-                return x;
-            }));
+        // return this.http.delete(`${environment.apiUrl}/users/${id}`)
+        //     .pipe(map(x => {
+        //         // auto logout if the logged in user deleted their own record
+        //         if (id == this.userValue?.id) {
+        //             this.logout();
+        //         }
+        //         return x;
+        //     }));
     }
 }
